94cd104255bda4ed6d785b9eca48b1a050b5a5bd
AdamMavr <tommaso140413@gmail.com>
2022-11-20 02:46:25 +0300

    Renaming Classes class to ClassesAndObjects class Adding methods to the ClassesAndObjects class

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,23 +1,27 @@
 // публичный класс это главный класс, и он может быть только один. но может быть несколько обычных классов. класс это тип данных который мы создаём сами.
-public class Classes {
+public class ClassesAndObjects {
     public static void main (String[] args) {
         Person adam = new Person();
         adam.name = "Adam";
         adam.age = 24;
         adam.prof = "dev";
         adam.car = false;
+        adam.greeting();
+
 
         Person vella = new Person();
         vella.name = "Vella";
         vella.age = 27;
         vella.prof = "designer";
         vella.car = false;
+        vella.greeting();
 
         Person elena = new Person();
         elena.name = "Elena";
         elena.age = 50;
         elena.prof = "farm";
         elena.car = true;
+        elena.sayHi();
     }
 }
 
@@ -26,4 +30,12 @@ class Person {
     int age;
     String prof;
     boolean car;
+
+    void greeting() {
+        System.out.println("Меня зовут " + name + ", мне " + age + " лет." + " Моя профессия " + prof);
+    }
+
+    void sayHi() {
+        System.out.println("Привет");
+    }
 }
