f09ade461216b32779948a09895f1148f5ee7369
AdamMavr <tommaso140413@gmail.com>
2022-11-21 19:06:57 +0300

    Adding method setMainInfo to the ClassesAndObjects class, and removing little methods

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -2,32 +2,29 @@
 public class ClassesAndObjects {
     public static void main (String[] args) {
         Person firstPerson = new Person();
-        firstPerson.setMainInfo("Adam", 24, "dev");
+        firstPerson.setMainInfo("Адам", 24);
         firstPerson.greeting();
-
-
-        Person secondPerson = new Person();
-        secondPerson.setMainInfo("Vella", 27, "techsup");
-        secondPerson.greeting();
-
-        Person thirdPerson = new Person();
-        thirdPerson.setMainInfo("Elea", 51, "pharm");
-        thirdPerson.greeting();
+        System.out.println(firstPerson.getName());
+        System.out.println(firstPerson.getAge());
     }
 }
 
 class Person {
-    String name;
-    int age;
-    String prof;
-
-    void setMainInfo(String username, int userage, String userprof) {
+    private String name;
+    private int age;
+    public void setMainInfo(String username, int userage) {
         name = username;
         age = userage;
-        prof = userprof;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
     }
 
     void greeting() {
-        System.out.println("Меня зовут " + name + ", мне " + age + " лет." + " Моя профессия " + prof);
+        System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
     }
 }
