4e4f7bced70e2aa0c32c752b50a2dc2e154642bf
AdamMavr <tommaso140413@gmail.com>
2022-11-21 17:54:54 +0300

    Adding method setMainInfo to the ClassesAndObjects class, and removing little methods

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,27 +1,18 @@
 // публичный класс это главный класс, и он может быть только один. но может быть несколько обычных классов. класс это тип данных который мы создаём сами.
 public class ClassesAndObjects {
     public static void main (String[] args) {
-        Person adam = new Person();
-        adam.name = "Adam";
-        adam.age = 24;
-        adam.prof = "dev";
-        adam.car = false;
-        adam.greeting();
+        Person firstPerson = new Person();
+        firstPerson.setMainInfo("Adam", 24, "dev");
+        firstPerson.greeting();
 
 
-        Person vella = new Person();
-        vella.name = "Vella";
-        vella.age = 27;
-        vella.prof = "designer";
-        vella.car = false;
-        vella.greeting();
+        Person secondPerson = new Person();
+        secondPerson.setMainInfo("Vella", 27, "techsup");
+        secondPerson.greeting();
 
-        Person elena = new Person();
-        elena.name = "Elena";
-        elena.age = 50;
-        elena.prof = "farm";
-        elena.car = true;
-        elena.sayHi();
+        Person thirdPerson = new Person();
+        thirdPerson.setMainInfo("Elea", 51, "pharm");
+        thirdPerson.greeting();
     }
 }
 
@@ -29,13 +20,14 @@ class Person {
     String name;
     int age;
     String prof;
-    boolean car;
+
+    void setMainInfo(String username, int userage, String userprof) {
+        name = username;
+        age = userage;
+        prof = userprof;
+    }
 
     void greeting() {
         System.out.println("Меня зовут " + name + ", мне " + age + " лет." + " Моя профессия " + prof);
-    }
-
-    void sayHi() {
-        System.out.println("Привет");
     }
 }
