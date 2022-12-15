2aad82059e8b4241eaaf846f0f533446112d3b95
AdamMavr <tommaso140413@gmail.com>
2022-12-15 17:01:59 +0300

    Interfaces

---
+++ src/Interfaces/Info.java
@@ -0,0 +1,7 @@
+// у интерфейса не может быть тела.
+
+package Interfaces;
+
+public interface Info {
+    public void showInfo();
+}
---
+++ src/Interfaces/Test.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public class Test {
+    public static void main(String[] args) {
+        Animal fAnimal = new Animal(1);
+        Person fPerson = new Person("Adam");
+        fAnimal.showInfo();
+        fPerson.showInfo();
+    }
+}
